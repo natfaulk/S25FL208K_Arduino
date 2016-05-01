e8a6ba9b9e5640504db1e9ea7410f0b4919250f6
natfaulk <nfaulk@hotmail.co.uk>
2016-05-01 20:45:00 +1200

    working test code to check SPI flash chip connected

--- spi_flash.ino
+++ spi_flash.ino
@@ -7,12 +7,51 @@ void setup(){
   Serial.begin(115200);
   // if this is run before setup, then doesn't work.
   // other option is to have an init function.
-  spiFlash = new S25FL208K(4);
+  //spiFlash = new S25FL208K(4);
   Serial.println("Setup complete.");
 
-  Serial.println(spiFlash->readStatus(),BIN);
-  spiFlash->writeEnable();
-  Serial.println(spiFlash->readStatus(),BIN);
+  //Serial.println(spiFlash->readStatus(),BIN);
+  //spiFlash->writeEnable();
+  //Serial.println(spiFlash->readStatus(),BIN);
+
+  SPI.begin();
+  int csPin = 4;
+  pinMode(csPin, OUTPUT);
+  digitalWrite(csPin, 1);
+
+  uint8_t res = 0;
+  digitalWrite(csPin, 0);
+  SPI.transfer(0x05);
+  res = SPI.transfer(0);
+  digitalWrite(csPin, 1);
+
+  Serial.println(res,BIN);
+
+  digitalWrite(csPin, 0);
+  SPI.transfer(0x06);
+  digitalWrite(csPin, 1);
+
+  digitalWrite(csPin, 0);
+  SPI.transfer(0x05);
+  res = SPI.transfer(0);
+  digitalWrite(csPin, 1);
+
+  Serial.println(res,BIN);
+
+  digitalWrite(csPin, 0);
+  SPI.transfer(0x04);
+  digitalWrite(csPin, 1);
+
+  digitalWrite(csPin, 0);
+  SPI.transfer(0x05);
+  res = SPI.transfer(0);
+  digitalWrite(csPin, 1);
+
+  Serial.println(res,BIN);
+
+
+
+
 }
 
 void loop(){
