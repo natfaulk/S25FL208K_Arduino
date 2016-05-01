678cd25127e42570db48b7f865cb355ae2f0fef7
natfaulk <nfaulk@hotmail.co.uk>
2016-05-01 22:07:39 +1200

    Able to read and write data to flash chip

--- S25FL208K.cpp
+++ S25FL208K.cpp
@@ -8,25 +8,89 @@ S25FL208K::S25FL208K(uint8_t csPin){
   digitalWrite(mCsPin, 1); // active low, start disabled
 }
 
-uint8_t S25FL208K::sendCommand(uint8_t command){
-  uint8_t result = 0;
+// returns dynamic memory
+// therefore needs a delete[] call on the pointer when done
+uint8_t* S25FL208K::sendCommand(uint8_t command, uint8_t bytesToRead){
+  uint8_t* result = NULL;
   digitalWrite(mCsPin, 0);
-  result = SPI.transfer(command);
+  SPI.transfer(command);
+  if (bytesToRead > 0){
+    result = new uint8_t[bytesToRead];
+    for (int i = 0; i<bytesToRead; i++){
+      result[i] = SPI.transfer(DUMMY_VALUE);
+    }
+  }
   digitalWrite(mCsPin, 1);
   return result;
 }
 
 uint8_t S25FL208K::readStatus(){
-  sendCommand(READ_STATUS_REG);
-  return sendCommand(DUMMY_VALUE);
-}
-
-uint8_t S25FL208K::readData(uint32_t address, uint32_t numBytes){
-  sendCommand(READ_DATA);
-  // send 24bit address, continuosly read
-  return sendCommand(DUMMY_VALUE);// dont use. is a placeholder!
+  uint8_t* result = NULL;
+  uint8_t output = 0;
+  result = sendCommand(READ_STATUS_REG, 1);
+  output = result[0];
+  delete[] result;
+  return output;
 }
 
 void S25FL208K::writeEnable(void){
   sendCommand(WRITE_ENABLE);
+}
+
+void S25FL208K::writeDisable(void){
+  sendCommand(WRITE_DISABLE);
+}
+
+bool S25FL208K::getWIP(void){
+  return (this->readStatus() & WIP_MASK);
+}
+
+bool S25FL208K::readData(uint32_t address, uint32_t numBytes, uint8_t* &result){
+  // check WIP bit
+  if (this->getWIP()){
+    return false;
+  }
+
+  digitalWrite(mCsPin, 0);
+  // transfer the address as 3 bytes
+  SPI.transfer(READ_DATA);
+  SPI.transfer(uint8_t(0xFF & (address>>16)));
+  SPI.transfer(uint8_t(0xFF & (address>>8)));
+  SPI.transfer(uint8_t(0xFF & address));
+
+  // read the data into result
+  for(int i = 0; i<numBytes; i++){
+    result[i] = SPI.transfer(DUMMY_VALUE);
+  }
+  // end transfer
+  digitalWrite(mCsPin, 1);
+  return true;
+}
+
+bool S25FL208K::storeData(uint32_t address, uint8_t numBytes, uint8_t* &data){
+  // check WIP bit
+  if (this->getWIP()){
+    return false;
+  }
+
+  // check page erased
+
+
+  // set WRITE_ENABLE
+  this->writeEnable();
+
+  digitalWrite(mCsPin, 0);
+  // send address
+  SPI.transfer(PAGE_PROGRAM);
+  SPI.transfer(uint8_t(0xFF & (address>>16)));
+  SPI.transfer(uint8_t(0xFF & (address>>8)));
+  SPI.transfer(uint8_t(0xFF & address));
+  //send data
+  for(int i = 0; i<numBytes; i++){
+    SPI.transfer(data[i]);
+  }
+
+  digitalWrite(mCsPin, 1);
+
+  return true;
 }
--- S25FL208K.h
+++ S25FL208K.h
@@ -19,16 +19,23 @@ const uint8_t RELEASE_POWER_DOWN = 0xAB;
 const uint8_t DEV_ID = 0x90;
 const uint8_t JEDEC_ID = 0x9F;
 const uint8_t DUMMY_VALUE = 0x00;
-
+const uint8_t WIP_MASK = 0x01;
 
 class S25FL208K{
 public:
   S25FL208K(uint8_t csPin);
-  uint8_t sendCommand(uint8_t command);
   uint8_t readStatus();
-  uint8_t readData(uint32_t address, uint32_t numBytes);
+  bool readData(uint32_t address, uint32_t numBytes, uint8_t* &result);
   void writeEnable(void);
+  void writeDisable(void);
+  bool getWIP(void);
+  // address is 24 bytes, however no uint24_t data type
+  // up to 255 bytes (chip supports up to 256 at once but want to use a uint8_t)
+  bool storeData(uint32_t address, uint8_t numBytes, uint8_t* &data);
 private:
+  // returns dynamic memory
+  // therefore needs a delete[] call on the pointer when done
+  uint8_t* sendCommand(uint8_t command, uint8_t bytesToRead = 0);
   uint8_t mCsPin;
 };
 
